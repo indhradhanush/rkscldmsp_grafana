50a4e95c79644138896a0256ba7d8dbce6fe0041
Karthikeyan Krish <indhradhanush@gmail.com>
2023-04-19 13:45:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
 
 setup(
     name='rkscldmsp_grafana',
-    version='0.1.0',
+    version='0.1.1',
     description='Ruckus MSP Cloud monitoring with Grafana',
     long_description=readme,
     author='Karthikeyan Krish',
