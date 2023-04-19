142683adfe9c6359eceb9d59d5d16ca390e2b95b
Karthikeyan Krish <indhradhanush@gmail.com>
2023-04-19 13:54:10 +0530

    Update countdata.py

--- src/countdata.py
+++ src/countdata.py
@@ -10,9 +10,20 @@ from collections import Counter
 disconnectedcount = 0
 
 def getapcategories(r2):
-    c = Counter(player['deviceStatus'] for player in r2)
-    c = dict(c)
-    return(c)
+    #c = Counter(player['deviceStatus'] for player in r2)
+    #c = dict(c)
+    dev_status_list = []
+    for x in range(0, len(r2)):
+        if "deviceStatus" in r2[x]:
+            #print("Exists")
+            dev_status_list.append(r2[x]['deviceStatus'])
+        else:
+            #print("Does not exist")
+            dev_status_list.append("Unknown")
+    print(dev_status_list)
+    count_dev = dict(Counter(dev_status_list))
+    print(count_dev)
+    return(count_dev)
 
 def getcustomers(r2):
     c = Counter(player['customerName'] for player in r2)
